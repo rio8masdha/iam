309ce20664c4cfff4ec6d44d3188047d60262adf
rio8masdha <112617780+rio8masdha@users.noreply.github.com>
2022-10-13 16:50:28 +0700

    Update and rename variable.tf to variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
-#variable "access_key" {
-#     description = "Access key to AWS console"
-#}
-#variable "secret_key" {
-#     description = "Secret key to AWS console"
-#}
-#variable "region" {
-#     description = "Region of AWS VPC"
-#}
+variable "access_key" {
+     description = "Access key to AWS console"
+}
+variable "secret_key" {
+     description = "Secret key to AWS console"
+}
+variable "region" {
+     description = "Region of AWS VPC"
+}
 variable "name" {
   default = "myadmin"
   type        = "string"
