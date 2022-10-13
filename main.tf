31d438a7b1fee738277470cf5541a968ad27b0f3
rio8masdha <112617780+rio8masdha@users.noreply.github.com>
2022-10-13 17:01:40 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
+#provider "aws" {
+#      region     = "${var.region}"
+#      access_key = "${var.access_key}"
+#      secret_key = "${var.secret_key}"
+#}
+
 provider "aws" {
-      region     = "${var.region}"
-      access_key = "${var.access_key}"
-      secret_key = "${var.secret_key}"
+  region  = "us-east-1"
+  access_key = var.access_id_iam
+  secret_key = var.secret_iam
 }
+
 
 resource "aws_iam_user" "user" {
   name = var.name
