91e55b21e65d5d967c75f92d35326c10330661c6
rio8masdha <112617780+rio8masdha@users.noreply.github.com>
2022-10-13 16:46:06 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,12 @@
 #      secret_key = "${var.secret_key}"
 #}
 
+provider "aws" {
+  region  = "us-east-1"
+  access_key = var.access_id
+  secret_key = var.secret
+}
+
 variable "access_id_iam" {
   default = ""
 }
