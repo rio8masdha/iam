794cc354268b5d4cdd643e6b96c606941d25ae6d
rio8masdha <112617780+rio8masdha@users.noreply.github.com>
2022-10-13 16:33:24 +0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,3 +5,19 @@ variable "access_id_iam" {
 variable "secret_iam" {
   default = ""
 }
+
+variable "us-east-1" {
+}
+
+variable "name" {
+  default = "myadmin"
+  type        = "string"
+  description = "The name of the user"
+}
+
+variable "policy_arns" {
+  default = "arn:aws:iam::aws:policy/AdministratorAccess"
+  type        = string
+  description = "ARN of policy to be associated with the created IAM user"
+}
+
