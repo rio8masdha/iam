0eb64f9a10c4682ca5e89eeecc87429d9be6f8e2
rio8masdha <112617780+rio8masdha@users.noreply.github.com>
2022-10-13 17:05:18 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,17 @@
 #      secret_key = "${var.secret_key}"
 #}
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
 provider "aws" {
   region  = "us-east-1"
   access_key = var.access_id_iam
