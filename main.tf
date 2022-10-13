adfec4864daaf17f10df3d94c11a99d59fa9e93c
rio8masdha <112617780+rio8masdha@users.noreply.github.com>
2022-10-13 17:10:11 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,27 @@
-#provider "aws" {
-#      region     = "${var.region}"
-#      access_key = "${var.access_key}"
-#      secret_key = "${var.secret_key}"
-#}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
-  region  = "us-east-1"
-  access_key = var.access_id_iam
-  secret_key = var.secret_iam
+  region = "eu-west-1"
 }
 
+module "iam-user" {
+  source = "../"
 
-resource "aws_iam_user" "user" {
-  name = var.name
+  name        = "iam-user"
+  environment = "test"
+  label_order = ["name", "environment"]
+
+  policy_enabled          = true
+  policy                  = data.aws_iam_policy_document.default.json
+  pgp_key                 = ""
+  password_length         = 20
+  password_reset_required = true
 }
 
-resource "aws_iam_user_policy_attachment" "attach-user" {
-  user       = "${aws_iam_user.user.name}"
-  policy_arn = var.policy_arns
+data "aws_iam_policy_document" "default" {
+  statement {
+    actions = [
+      "ec2:Describe*"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
 }
